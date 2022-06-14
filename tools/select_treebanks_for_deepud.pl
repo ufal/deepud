3d772c43519f8bf5ea51c5e360f53a186b3a84de
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-06-14 11:01:43 +0200

    Minor.

--- tools/select_treebanks_for_deepud.pl
+++ tools/select_treebanks_for_deepud.pl
@@ -11,8 +11,8 @@ binmode(STDERR, ':utf8');
 # From the UD tools repository:
 use udlib;
 
-# This should later become a commandline argument.
-my $path = '/net/data/universal-dependencies-2.10'; ###!!!/UD_*/README*
+###!!! This should later become a commandline argument.
+my $path = '/net/data/universal-dependencies-2.10';
 my @folders = udlib::list_ud_folders($path);
 my $n = scalar(@folders);
 my $n_notext = 0;
@@ -37,7 +37,7 @@ foreach my $folder (@folders)
         $n_selected++;
     }
 }
-print("$n total treebanks found\n");
+print("$n total treebanks found in $path\n");
 print("$n_notext treebanks omitted because the underlying text is not available\n");
 print("$n_nolemma treebanks omitted (out of those that have text) because they do not have lemmas\n");
 print("$n_selected treebanks remain for Deep UD\n");
