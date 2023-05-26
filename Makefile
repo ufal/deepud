e4a5db6febd6f94087e1189fa0f901ce6417c5f6
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-05-26 12:45:52 +0200

    Improved collection of UD treebanks for Deep UD.

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 SHELL=/bin/bash
-RELEASE=2.10
+RELEASE=2.12
 UDSRCDIR=/net/data/universal-dependencies-$(RELEASE)
-UDEXCLUDE=UD_Arabic-NYUAD UD_Arabic-PUD UD_Beja-NSC UD_Chinese-CFL UD_Chinese-HK UD_Chinese-PUD UD_Chukchi-HSE UD_English-ESL UD_English-GUMReddit UD_French-FTB UD_French-PUD UD_Frisian_Dutch-Fame UD_Hindi-PUD UD_Hindi_English-HIENCS UD_Italian-PUD UD_Japanese-BCCWJ UD_Japanese-BCCWJLUW UD_Korean-PUD UD_Maltese-MUDT UD_Mbya_Guarani-Dooley UD_Old_French-SRCMF UD_Old_Turkish-Tonqq UD_Portuguese-PUD UD_Spanish-PUD UD_Swedish_Sign_Language-SSLC UD_Swiss_German-UZH UD_Thai-PUD
 # Every time we need to check the newly added treebanks. Do they have lemmas? Do they have text?
 # Occasionally we should also check the previously excluded treebanks. Maybe lemmas have been added to some of them?
-#   tools/select_treebanks_for_deepud.pl
+# Run the following command from the Deep UD repository and paste here the UDEXCLUDE assignment that it generates.
+#   tools/select_treebanks_for_deepud.pl --udpath /net/data/universal-dependencies-2.12
+UDEXCLUDE=UD_Arabic-NYUAD UD_Arabic-PUD UD_Beja-NSC UD_Chinese-CFL UD_Chinese-HK UD_Chinese-PUD UD_Chukchi-HSE UD_English-ESL UD_English-GUMReddit UD_French-FTB UD_French-PUD UD_Frisian_Dutch-Fame UD_Hindi-PUD UD_Hindi_English-HIENCS UD_Italian-PUD UD_Japanese-BCCWJ UD_Japanese-BCCWJLUW UD_Korean-PUD UD_Maltese-MUDT UD_Mbya_Guarani-Dooley UD_Old_French-SRCMF UD_Old_Turkish-Tonqq UD_Portuguese-PUD UD_Spanish-PUD UD_Swedish_Sign_Language-SSLC UD_Swiss_German-UZH UD_Thai-PUD
 CORENLPDIR=/net/work/people/droganova/CoreNLP
 
 # It is recommended to run make all |& tee make.log
--- tools/select_treebanks_for_deepud.pl
+++ tools/select_treebanks_for_deepud.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # Checks a UD release for treebanks that are suitable to be included in DeepUD.
-# Copyright © 2022 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# Copyright © 2022, 2023 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # License: GNU GPL
 
 use utf8;
@@ -8,12 +8,27 @@ use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
+use Getopt::Long;
 # From the UD tools repository:
 use udlib;
 
-###!!! This should later become a commandline argument.
-my $path = '/net/data/universal-dependencies-2.10';
-my @folders = udlib::list_ud_folders($path);
+sub usage
+{
+    print STDERR ("Usage: $0 --udpath /net/data/universal-dependencies-2.12\n");
+}
+
+my $udpath; # released data, e.g., at ÚFAL: /net/data/universal-dependencies-2.12
+GetOptions
+(
+    'udpath=s' => \$udpath
+);
+if(!defined($udpath))
+{
+    usage();
+    die("Unknown path to the released Universal Dependencies data");
+}
+
+my @folders = udlib::list_ud_folders($udpath);
 my $n = scalar(@folders);
 my $n_notext = 0;
 my $n_nolemma = 0;
@@ -21,7 +36,7 @@ my $n_selected = 0;
 my @exclude = ();
 foreach my $folder (@folders)
 {
-    my $metadata = udlib::read_readme($folder, $path);
+    my $metadata = udlib::read_readme($folder, $udpath);
     if($metadata->{'Includes text'} =~ m/^no/i)
     {
         $n_notext++;
@@ -37,9 +52,10 @@ foreach my $folder (@folders)
         $n_selected++;
     }
 }
-print("$n total treebanks found in $path\n");
+print("$n total treebanks found in $udpath\n");
 print("$n_notext treebanks omitted because the underlying text is not available\n");
 print("$n_nolemma treebanks omitted (out of those that have text) because they do not have lemmas\n");
 print("$n_selected treebanks remain for Deep UD\n");
 # We will need the list of treebanks to exclude for the Makefile.
+print("Replace the UDEXCLUDE variable assignment in the Makefile with the following:\n");
 print("UDEXCLUDE=".join(' ', @exclude)."\n");
